ceb48ce15d5d60c6e0a145b24f9a4ed4b6bdfa92
Hollydan <2642956839@qq.com>
2017-09-29 15:06:10 +0800

    deletecategory

--- src/Handlers/DeleteCategoryHandler.php
+++ src/Handlers/DeleteCategoryHandler.php
@@ -29,12 +29,26 @@ class DeleteCategoryHandler extends Handler
             'category_alias.required' => '请传入分类id',
         ]);
 
-        $alias = $this->request->get('category_alias');
-        $category = Category::where('alias', $alias)->first();
+        $category = Category::where('alias', $this->request->get('category_alias'))->first();
 
         if (!$category instanceof Category) {
             return $this->withCode(401)->withError('请重新确认分组id');
         }
+
+        //删除分类下面的所有分组信息
+        foreach ($category->groups() as $group) {
+            foreach ($group->pictures() as $picture) {
+                $complatePath = base_path('statics' . strstr($picture->path, '/uploads'));
+                if (file_exists($complatePath)) {
+                    rmdir($complatePath);
+                }
+            }
+            $groupPath = base_path('statics' . substr(strstr($group->pictures()->first()->path, '/uploads'), 0, strrpos(strstr($group->pictures()->first()->path, '/uploads'), '/')-1));
+            if (file_exists($groupPath)) {
+                rmdir($groupPath);
+            }
+        }
+
 
         if ($category->delete()) {
             return $this->withCode(200)->withMessage('删除分组信息成功');
