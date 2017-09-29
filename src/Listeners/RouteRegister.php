913a81329761471f3c5c8112b924fd98f696a344
Hollydan <2642956839@qq.com>
2017-09-29 17:44:42 +0800

    add router

--- src/Listeners/RouteRegister.php
+++ src/Listeners/RouteRegister.php
@@ -8,6 +8,8 @@
 
 namespace Notadd\Carious\Listener;
 
+use Notadd\Carousel\Controller\CategoryController;
+use Notadd\Carousel\Controller\GroupController;
 use Notadd\Carousel\Controller\PictureController;
 use Notadd\Foundation\Routing\Abstracts\RouteRegister as AbstractRouteRegister;
 
@@ -20,11 +22,27 @@ class RouteRegister extends AbstractRouteRegister
     public function handle()
     {
         $this->router->group(['middleware' => ['cross', 'web'], 'prefix' => 'api/carousel'], function () {
-            
+
             $this->router->group(['prefix' => 'picture'], function () {
                 $this->router->post('set', PictureController::class.'@set');
                 $this->router->post('delete', PictureController::class.'@delete');
                 $this->router->post('update', PictureController::class.'@update');
+            });
+
+            $this->router->group(['prefix' => 'group'], function () {
+                $this->router->post('all', GroupController::class.'@all');
+                $this->router->post('set', GroupController::class.'@set');
+                $this->router->post('delete', GroupController::class.'@delete');
+                $this->router->post('update', GroupController::class.'@update');
+                $this->router->post('show', GroupController::class.'@show');
+            });
+
+            $this->router->group(['prefix' => 'category'], function () {
+                $this->router->post('all', CategoryController::class.'@all');
+                $this->router->post('set', CategoryController::class.'@set');
+                $this->router->post('delete', CategoryController::class.'@delete');
+                $this->router->post('update', CategoryController::class.'@update');
+                $this->router->post('show', CategoryController::class.'@show');
             });
         });
     }
