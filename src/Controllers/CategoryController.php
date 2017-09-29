3ce0db21f6cae48213a0fa9e44b8ae9827d488c1
Hollydan <2642956839@qq.com>
2017-09-29 15:48:39 +0800

    categorycontroller

---
+++ src/Controllers/CategoryController.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * The file is part of Notadd
+ *
+ * @author: Hollydan<2642956839@qq.com>
+ * @copyright (c) 2017, notadd.com
+ * @datetime: 17-9-29 下午3:44
+ */
+
+namespace Notadd\Carousel\Controller;
+
+
+use Notadd\Carousel\Handler\AllCategoryHandler;
+use Notadd\Carousel\Handler\DeleteCategoryHandler;
+use Notadd\Carousel\Handler\SetCategoryHandler;
+use Notadd\Carousel\Handler\ShowCategoryHandler;
+use Notadd\Carousel\Handler\UpdateCategoryHandler;
+use Notadd\Foundation\Routing\Abstracts\Controller;
+
+class CategoryController extends Controller
+{
+    /**
+     * @param AllCategoryHandler $handler
+     * @return \Notadd\Foundation\Passport\Responses\ApiResponse|\Psr\Http\Message\ResponseInterface|\Zend\Diactoros\Response
+     */
+    public function all(AllCategoryHandler $handler)
+    {
+        return $handler->toResponse()->generateHttpResponse();
+    }
+
+    /**
+     * @param SetCategoryHandler $handler
+     * @return \Notadd\Foundation\Passport\Responses\ApiResponse|\Psr\Http\Message\ResponseInterface|\Zend\Diactoros\Response
+     */
+    public function set(SetCategoryHandler $handler)
+    {
+        return $handler->toResponse()->generateHttpResponse();
+    }
+
+    /**
+     * @param DeleteCategoryHandler $handler
+     * @return \Notadd\Foundation\Passport\Responses\ApiResponse|\Psr\Http\Message\ResponseInterface|\Zend\Diactoros\Response
+     */
+    public function delete(DeleteCategoryHandler $handler)
+    {
+        return $handler->toResponse()->generateHttpResponse();
+    }
+
+    /**
+     * @param UpdateCategoryHandler $handler
+     * @return \Notadd\Foundation\Passport\Responses\ApiResponse|\Psr\Http\Message\ResponseInterface|\Zend\Diactoros\Response
+     */
+    public function update(UpdateCategoryHandler $handler)
+    {
+        return $handler->toResponse()->generateHttpResponse();
+    }
+
+    /**
+     * @param ShowCategoryHandler $handler
+     * @return \Notadd\Foundation\Passport\Responses\ApiResponse|\Psr\Http\Message\ResponseInterface|\Zend\Diactoros\Response
+     */
+    public function show(ShowCategoryHandler $handler)
+    {
+        return $handler->toResponse()->generateHttpResponse();
+    }
+}
